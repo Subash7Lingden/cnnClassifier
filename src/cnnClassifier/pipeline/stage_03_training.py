6668c7a668f39ea50749c8a5244968abd985b92c
Subash7Lingden <subashsubbalingden@gmail.com>
2024-04-04 16:54:25 +0545

    final training added

---
+++ src/cnnClassifier/pipeline/stage_03_training.py
@@ -0,0 +1,17 @@
+from cnnClassifier.config import ConfigurationManager
+from cnnClassifier.components import Training
+from cnnClassifier import logger
+
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        training_config = config.get_training_config()
+        training = Training(config=training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train()
+    
